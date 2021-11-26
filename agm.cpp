5b434053417786723e1cecfe671b3f7121a8db37
Pierre Abbat <phma@bezitopo.org>
2021-11-26 16:07:52 +0000

    Handle d in agm1

--- agm.cpp
+++ agm.cpp
@@ -5,11 +5,18 @@
 /******************************************************/
 
 #include "agm.h"
+#include "angle.h"
+using namespace std;
 
 AgmRec agm1(AgmRec ag)
 {
   AgmRec ret;
+  complex<double> c;
   ret.a=(ag.a+ag.g)/2.;
   ret.g=sqrt(ag.a*ag.g);
+  c=ret.a*cossin(ag.d);
+  if (abs(c-ret.g)>abs(c+ret.g))
+    ret.g=-ret.g;
+  ret.d=argi(ret.g/ret.a);
   return ret;
 }
