3e0acf243c77a60b55e087a82bdcc3a87f98d347
Pierre Abbat <phma@bezitopo.org>
2021-11-28 07:38:32 +0000

    Define angles and run a few times around the branch point at 0

--- angle.h
+++ angle.h
@@ -16,4 +16,28 @@ double sin(int angle);
 double cos(int angle);
 std::complex<double> cossin(int angle);
 int argi(std::complex<double> z);
+
+#define SEC1 1657
+#define FURMAN1 32768
+#define MIN1 99421
+#define DEG1 5965232
+#define AT0512 0x80ae90e
+// AT0512 is arctangent of 5/12, 22.619865°
+#define DEG30 0x0aaaaaab
+#define AT34 0x0d1bfae2
+// AT34 is arctangent of 3/4, 36.8698976°
+#define DEG40 0x0e38e38e
+#define DEG45 0x10000000
+#define DEG50 0x11c71c72
+#define DEG60 0x15555555
+#define DEG72 0x1999999a
+#define DEG90 0x20000000
+#define DEG120 0x2aaaaaab
+#define DEG144 0x33333333
+#define DEG150 0x35555555
+#define DEG180 0x40000000
+#define DEG270 0x60000000
+#define DEG360 0x80000000
+#define PHITURN 0xcf1bbcdd
+
 #endif
--- main.cpp
+++ main.cpp
@@ -10,15 +10,24 @@
 #include "angle.h"
 using namespace std;
 
-int main(int argc,char **argv)
+void printag(AgmResult ag)
 {
-  AgmResult ag;
   int i;
-  ag=agm(cossin(0x3fffffff)*0.999999999,1);
   cout<<ag.m;
   for (i=0;i<ag.branch.length();i++)
     cout<<' '<<hex<<setw(2)<<(int)(ag.branch[i]&0xff)<<dec;
   cout<<endl;
+}
+
+int main(int argc,char **argv)
+{
+  AgmResult ag;
+  int i;
+  for (i=0;i<32;i++)
+  {
+    ag=agm(cossin(DEG45*i)*0.5,1,ag.branch);
+    printag(ag);
+  }
   return 0;
 }
   
