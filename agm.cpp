886db3fb05a82ceedcb9d1b22c4e62fb48226900
Pierre Abbat <phma@bezitopo.org>
2021-11-28 01:45:12 +0000

    Write agm function

--- agm.cpp
+++ agm.cpp
@@ -20,3 +20,30 @@ AgmRec agm1(AgmRec ag)
   ret.d=argi(ret.g/ret.a);
   return ret;
 }
+
+AgmResult agm(complex<double> a,complex<double> g,string branch)
+{
+  AgmResult ret;
+  AgmRec in,out;
+  size_t pos;
+  int i=0;
+  in.a=a;
+  in.g=g;
+  do
+  {
+    if (i<branch.length())
+      in.d=branch[i]<<24;
+    else
+      in.d=0;
+    out=agm1(in);
+    ret.branch+=(char)((out.d+0x800000)>>24);
+    ++i;
+  } while (i<branch.length() || (out.a!=out.g && out.g!=0.));
+  pos=ret.branch.find_last_not_of('\0');
+  if (pos<ret.branch.length())
+    ret.branch.erase(pos+1);
+  else
+    ret.branch.clear();
+  ret.m=out.g;
+  return ret;
+}
--- agm.h
+++ agm.h
@@ -6,6 +6,7 @@
 
 #include <cmath>
 #include <complex>
+#include <string>
 
 struct AgmRec
 {
@@ -13,4 +14,11 @@ struct AgmRec
   int32_t d;
 };
 
+struct AgmResult
+{
+  std::complex<double> m;
+  std::string branch;
+};
+
 AgmRec agm1(AgmRec ag);
+AgmResult agm(std::complex<double> a,std::complex<double> g=1,std::string branch="");
