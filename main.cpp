86c8ea477d7c93f0a0cc64cca52e4bcbc66a38aa
Pierre Abbat <phma@bezitopo.org>
2021-12-03 18:37:48 +0000

    Draw the curve in PostScript
    It looks messed up.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <iomanip>
 #include "agm.h"
 #include "angle.h"
+#include "ps.h"
 using namespace std;
 
 void printag(AgmResult ag)
@@ -22,12 +23,22 @@ void printag(AgmResult ag)
 int main(int argc,char **argv)
 {
   AgmResult ag;
+  PostScript ps;
   int i;
+  ps.open("agm.ps");
+  ps.setpaper(papersizes["A4 landscape"],0);
+  ps.prolog();
+  ps.startpage();
+  ps.setscale(-2,-1.5,2,1.5,0);
+  ps.setcolor(0,0,1);
+  ps.startline();
   for (i=0;i<32;i++)
   {
     ag=agm(cossin(DEG45*i)*0.5,1,ag.branch);
+    ps.lineto(ag.m);
     printag(ag);
   }
+  ps.endline();
   return 0;
 }
   
