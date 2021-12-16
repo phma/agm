c89d43651f519f1615c0abbbb0ebc601c8c64b26
Pierre Abbat <phma@bezitopo.org>
2021-12-16 10:09:49 +0000

    Draw an AGM bean

--- main.cpp
+++ main.cpp
@@ -6,10 +6,31 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <cfloat>
 #include "agm.h"
 #include "angle.h"
 #include "ps.h"
 using namespace std;
+
+const signed char c65[]=
+{
+  65,63,60,56,52,39,33,25,16,
+  0,-16,-25,-33,-39,-52,-56,-60,-63,
+  -65,-63,-60,-56,-52,-39,-33,-25,-16,
+  0,16,25,33,39,52,56,60,63
+};
+
+vector<complex<double> > tinyCircle(complex<double> center)
+/* Returns a circle with radius 65 ulps. center must be between 2 and -2,
+ * exclusive, or the result will be inaccurate.
+ */
+{
+  vector<complex<double> > ret;
+  int i;
+  for (i=0;i<36;i++)
+    ret.push_back(center+complex<double>(c65[i],c65[(i+27)%36])*DBL_EPSILON);
+  return ret;
+}
 
 void printag(AgmResult ag)
 {
@@ -24,7 +45,7 @@ int main(int argc,char **argv)
 {
   AgmResult ag;
   PostScript ps;
-  vector<complex<double> > lattice;
+  vector<complex<double> > lattice,bean;
   int i;
   ps.open("agm.ps");
   ps.setpaper(papersizes["A4 landscape"],0);
@@ -77,6 +98,16 @@ int main(int argc,char **argv)
   for (i=0;i<lattice.size();i++)
   {
     ps.dot(lattice[i]);
+  }
+  ps.endpage();
+  ps.startpage();
+  ps.setcolor(0,0,1);
+  bean=tinyCircle(1);
+  for (i=0;i<4;i++)
+    bean=agmExpand(bean);
+  for (i=0;i<bean.size();i++)
+  {
+    ps.dot(bean[i]);
   }
   ps.endpage();
   cout<<pvAgm(pvAgm(2,3),pvAgm(5,7))<<' ';
